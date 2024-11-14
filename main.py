84b0cff66b98807b21966cbe5f4d4bc726a2a5e5
Vignesh Elumalai <vigneshoffl36@gmail.com>
2024-11-14 14:16:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import mysql.connector
 import boto3
+import psycopg2
 
 #Establishing the connection
 s3=boto3.client('s3')
-data = pd.read_csv(r'/home/beast/Downloads/used_cars_data.csv')
-conn = mysql.connector.connect(database='data', user='root', password='@Beast00036@')
-cursor=conn.cursor()
+data = pd.read_csv('{path_to_your_data_file}')
+conn = mysql.connector.connect(database='database_name', user='user_name', password='user_password')
+mysql_cursor=conn.cursor()
+redshift_conn = psycopg2.connect(host='redshift_endpoint', database='database_name', user='user_name', password='user_password', port='port_number')
+redshift_cursor=redshift_conn.cursor()
 
 #Data cleansing
 data['Power']=data['Power'].str.replace(' bhp', '', regex=False)
@@ -44,27 +47,44 @@ data['Price']=price
 
 #Creating table in MySql
 create_table_statement='''
-       CREATE TABLE IF NOT EXISTS cars (S_No int primary key, Name varchar(50), Location varchar(50), Year int, Kilometers_Driven int,
+       CREATE TABLE IF NOT EXISTS cars (S_No int primary key, Name varchar(100), Location varchar(50), Year int, Kilometers_Driven int,
        Fuel_Type varchar(50), Transmission varchar(50), Owner_Type varchar(50), Mileage float, Engine int, Power float, 
-       Seats int, New_Price int, Price int);
+       Seats float, New_Price int, Price int);
 '''
+mysql_cursor.execute(create_table_statement)
+redshift_cursor.execute(create_table_statement)
+redshift_conn.commit()
+conn.commit()
 
 #Insert statement for inserting a values
 insert_statement='''
         insert IGNORE into cars values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
 '''
-cursor.execute(create_table_statement)
-conn.commit()
 
 #Converting values into tuple and inserting into table
 for record in [tuple(x) for x in data.to_numpy()]:
-    cursor.execute(insert_statement, record)
+    mysql_cursor.execute(insert_statement, record)
     conn.commit()
 
 #Exporting the data
-data.to_csv('/media/beast/Beast/DE/Python_programms/2_OG/data/data.csv', index=False)
+data.to_csv('path_to_your_output_destination', index=False)
 
 #Uploading to s3
-file_path='/media/beast/Beast/DE/Python_programms/2_OG/data/data.csv'
+file_path='path_to_your_output_file'
 s3_path='data.csv'
-s3.upload_file(file_path, 'storage-for-mysql', s3_path)
+s3.upload_file(file_path, 'bucket_name', s3_path)
+
+#loading data into redshift table
+copy_statement=""" 
+    COPY dev.public.cars
+    FROM 'path_to_your_file' 
+    IAM_ROLE 'iam_role_arn' 
+    FORMAT AS CSV 
+    DELIMITER ',' 
+    QUOTE '"' 
+    IGNOREHEADER 1 
+    REGION AS 'region';
+ """
+
+#saving the changes
+redshift_conn.commit()
